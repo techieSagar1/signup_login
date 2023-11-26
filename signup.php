2edf7d2ad347426972d3de329670fdcd09a65e2f
techieSagar1 <146768350+techieSagar1@users.noreply.github.com>
2023-11-26 19:28:29 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,41 @@
+<?php
+
+    $server = 'localhost';
+    $user_name = 'root';
+    $password = '';
+    $dbname = 'student';
+
+
+    $connect = mysqli_connect($server,$user_name,$password,$dbname);
+
+
+    if(!$connect){
+        die("failed" .mysqli_connect_error());
+    }
+
+?>
+    
+
+<?php
+
+    $username= $_POST['username'];
+    $email = $_POST['email'];
+    $password= $_POST['password'];
+
+
+    $sql = "INSERT INTO signup (username,email,password) VALUES ('$username','$email','$password')";
+
+
+    $result = mysqli_query($connect,$sql);
+
+   
+    if($result){
+        echo "successful";
+    }
+    else{
+        echo "error";
+    }
+
+    mysqli_close($connect);
+
+?>
